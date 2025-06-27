77acc0ed645f7957c63291b6db65297bc6ab3921
Pooja-154 <poojabellapu@gmail.com>
2025-06-27 21:10:55 +0530

    Create CalculatorTest.java

---
+++ Week2/JUnit5/Ex4/CalculatorTest.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+    }
+
+    @Test
+    public void testAdd() {
+        int a = 10;
+        int b = 5;
+        int result = calculator.add(a, b);
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void testMultiply() {
+        int a = 4;
+        int b = 6;
+        int result = calculator.multiply(a, b);
+        assertEquals(24, result);
+    }
+}
