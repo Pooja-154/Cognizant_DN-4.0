9e3d349fc061eea0913d502e6620089c741fa223
Pooja-154 <poojabellapu@gmail.com>
2025-07-10 00:14:59 +0530

    Create CountryController.java

---
+++ Week4/Ex5/scr/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,26 @@
+package com.cognizant.springlearn.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.cognizant.model.Country;
+import com.cognizant.springlearn.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        LOGGER.info("START getCountry()");
+        Country country = countryService.getCountry(code);
+        LOGGER.info("END getCountry()");
+        return country;
+    }
+}
