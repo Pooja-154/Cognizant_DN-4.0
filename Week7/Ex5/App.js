5331bb587e281aa4f950ebd9947ba5fd6c8b3512
Pooja-154 <poojabellapu@gmail.com>
2025-08-01 23:13:59 +0530

    Create App.js

---
+++ Week7/Ex5/App.js
@@ -0,0 +1,14 @@
+const books = [
+  { title: "React Explained", author: "Zac Gordon" },
+  { title: "Eloquent JavaScript", author: "Marijn Haverbeke" }
+];
+
+const blogs = [
+  { id: 1, title: "Getting Started with React", content: "Learn the basics of React.js..." },
+  { id: 2, title: "Hooks Deep Dive", content: "Understand useState and useEffect..." }
+];
+
+const courses = [
+  { name: "React Fundamentals", code: "REACT101" },
+  { name: "Advanced React Patterns", code: "REACT201" }
+];
