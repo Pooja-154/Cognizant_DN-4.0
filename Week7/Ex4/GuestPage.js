1ac602fbf534d3a1b24d3d0bdde8d90f7bfe58dd
Pooja-154 <poojabellapu@gmail.com>
2025-08-01 23:11:23 +0530

    Create GuestPage.js

---
+++ Week7/Ex4/GuestPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const GuestPage = () => {
+  return (
+    <div>
+      <h2>Welcome Guest</h2>
+      <p>Please log in to book your flight tickets.</p>
+      <h3>Available Flights:</h3>
+      <ul>
+        <li>Flight A - Bangalore to Delhi</li>
+        <li>Flight B - Mumbai to Chennai</li>
+        <li>Flight C - Hyderabad to Kolkata</li>
+      </ul>
+    </div>
+  );
+};
+
+export default GuestPage;
